b7d8d9dc05ea2684e7e702d69e6223af7f999676
Sean Batzel <romulus108@protonmail.com>
2022-05-27 19:56:22 -0400

    Added a title block.

--- src/views/components/AddContact/AddContact.jsx
+++ src/views/components/AddContact/AddContact.jsx
@@ -1,12 +1,17 @@
 function AddContact() {
     return (
         <div>
-            <textarea name="first_name" />
-            <textarea name="last_name" />
-            <textarea name="blockchain_id" />
-            <div style={{ flexDirection: "row" }}>
-                <button name="submit" />
-                <button name="cancel" />
+            <div>
+                <h1>Add Contact</h1>
+            </div>
+            <div>
+                <textarea name="first_name" />
+                <textarea name="last_name" />
+                <textarea name="blockchain_id" />
+                <div style={{ flexDirection: "row" }}>
+                    <button name="submit" />
+                    <button name="cancel" />
+                </div>
             </div>
         </div>
     );
