62ffe1b151b660d8c35c4a45d233b10283b6ce58
Isaac Adams <huskydev@pm.me>
2022-05-27 17:14:43 -0400

    Rudimentary Navigation (#6)
    * basic routing and nav
    
    * Started scaffolding for the ContactsList, Inbox, and Navigation views.
    
    * basic routing and nav
    
    * package-lock
    
    * adding just a little bit of styling
    
    Co-authored-by: Sean Batzel <romulus108@protonmail.com>

--- src/views/pages/Navigation/Navigation.jsx
+++ src/views/pages/Navigation/Navigation.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
+import './Navigation.styles.css';
 import { Link } from 'react-router-dom';
 
 function Navigation() {
-    return (
-        <div>
-            <li>
-                <Link to="/">Home</Link>
-            </li>
-        </div>
-    );
+  return (
+    <nav
+      style={{
+        borderBottom: 'solid 1px',
+        paddingBottom: '1rem',
+      }}
+    >
+      <a>brand</a>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/profile">Profile</Link>
+        </li>
+        <li>
+          <Link to="/inbox">Inbox</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Navigation;
