ec21d35c7c887b27c70ae4a4898a4ec827dc5eaf
Sean Batzel <romulus108@protonmail.com>
2022-06-01 15:52:05 -0400

    Started scaffolding for the ContactsList, Inbox, and Navigation views. (#2)
    * Started scaffolding for the ContactsList, Inbox, and Navigation views.
    
    * Prepped for Docker.
    
    * Grant2/milestone1/romulus10/create message view (#5)
    
    * Started a view for the message creation form.
    
    * Corrected the CreateMessage view's name.
    
    * Grant2/milestone1/romulus10/inbox view (#4)
    
    * Created a ListView and started with storybook.
    
    * Added the .storybook directory.
    
    * Rudimentary Navigation (#6)
    
    * basic routing and nav
    
    * Started scaffolding for the ContactsList, Inbox, and Navigation views.
    
    * basic routing and nav
    
    * package-lock
    
    * adding just a little bit of styling
    
    Co-authored-by: Sean Batzel <romulus108@protonmail.com>
    
    * Started a basic AddContact view.
    
    * Added a title block.
    
    * Added a configurable splash screen view and fixed the ListView so it actually exported something.
    
    * Added a few Node versions to monitor.
    
    * Adjusted package-lock.
    
    * Added CodeQL anaysis.
    
    * Started mapping out Fennel Wallet management.
    
    * Started scaffolding a few more views.
    
    * Exporting AddContact correctly.
    
    * Moved `src/stories`
    
    * Splash screen with functions!
    
    * Update src/App.jsx
    
    Co-authored-by: Isaac Adams <huskydev@pm.me>
    
    * Some adjustments per Isaac's suggestions.
    
    * Removing 12.x.
    
    Co-authored-by: Isaac Adams <huskydev@pm.me>

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { useState, useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Outlet,
+} from 'react-router-dom';
+import './App.css';
+import Navigation from './views/pages/Navigation/Navigation';
+import Profile from './views/pages/Profile/Profile';
+import Inbox from './views/pages/Inbox/Inbox';
+import Home from './views/pages/Home';
+
+function LoadingMessage() {
+  return (
+    <div className="splash-screen">
+      Wait a moment while we load your app.
+      <div className="loading-dot">.</div>
+    </div>
+  );
+}
+
+function App() {
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
+  if (loading) { return LoadingMessage(); }
+  else {
+    return (
+      <Router>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/inbox" element={<Inbox />} />
+        </Routes>
+        <Outlet />
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ src/views/pages/GenerateWallet/GenerateWallet.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function GenerateWallet() {
+  return (
+    <div>
+      <h1>Generate Wallet</h1>
+    </div>
+  );
+};
+
+export default GenerateWallet;
