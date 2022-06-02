5c8354e7b8c7c5c5e1da085b24418fbc8016bfac
Sean Batzel <romulus108@protonmail.com>
2022-06-01 20:06:18 -0400

    Worked out some adjustments I discussed with @isaacadams today.

---
+++ src/views/components/LoadingScreen/index.js
@@ -0,0 +1,10 @@
+function LoadingScreen() {
+  return (
+    <div className="splash-screen">
+      Wait a moment while we load your app.
+      <div className="loading-dot">.</div>
+    </div>
+  );
+}
+
+export default LoadingScreen;
---
+++ src/views/pages/Inbox/index.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import ListView from '../../components/ListView';
+
+function Inbox() {
+  const [messageList, setMessageList] = useState([
+    { id: 1, name: "message1" },
+    { id: 2, name: "message2" },
+    { id: 3, name: "message3" },
+    { id: 4, name: "message4" },
+    { id: 5, name: "message5" }
+  ]);
+
+  return (
+    <div>
+      <h1>Inbox</h1>
+      <ListView itemList={messageList} />
+    </div>
+  );
+};
+
+export default Inbox;
