ea9b6c905b7c96dc2aaae673debb94e89db61135
Husky <huskydev@pm.me>
2022-06-02 20:39:52 -0400

    fixing eslint issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import {BrowserRouter as Router, Route, Routes, Outlet} from 'react-router-dom';
 import './App.css';
 import Navigation from './views/pages/Navigation';
