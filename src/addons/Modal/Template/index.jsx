dfae7b71669db87845fce4547ce7515adb8ef101
Isaac Adams <huskydev@pm.me>
2022-06-03 17:25:05 -0400

    Build issues (#13)
    * using parcel to see if that fixes issues
    
    * lock refresh

--- src/addons/Modal/Template/index.jsx
+++ src/addons/Modal/Template/index.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import {useContext} from 'react';
+import React, {useContext} from 'react';
 import {ModalContext} from '../useModalContext';
 import './Template.style.css';
 
