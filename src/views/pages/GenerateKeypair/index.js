bad501cc35fb006ea0d9d3d7fddf1a226a1a8a02
Ed Hertzog <edhertzog@gmail.com>
2022-06-06 21:47:44 -0400

    ws not working

--- src/views/pages/GenerateKeypair/index.js
+++ src/views/pages/GenerateKeypair/index.js
@@ -2,25 +2,31 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import CLI from '../../../services/CLI';
 import { CLI_URI } from '../../../config';
-import axios from 'axios';
+import { Client as WsClient } from 'rpc-websockets';
 
 function GenerateKeypair() {
 
   async function generateKeypair() {
+    
+    ///this will move to '../../../services/CLI'
     console.log('generate key pair');
-    const cli = new CLI();
-    const response = await axios.post(CLI_URI, {
-        jsonrpc: '2.0',
-        id: + new Date(),
-        method: 'eth_accounts',
-        params: {
-        },
-      }, {
-        headers: {
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': '*'
-        },
-      })
+
+    let ws = new WsClient(CLI_URI);
+
+    ws.on('open', async () => {
+      try {
+        //this fails:
+        await ws.call('help', []);
+        //this fails:
+        await ws.call('fennel-cli.exe help', []);
+        //Error generated:
+        //{code: -32601, message: 'Method not found'}
+      } catch (err) {
+        console.log(err);
+      }
+    });
+
+    console.log('done');
   }
 
   return (
