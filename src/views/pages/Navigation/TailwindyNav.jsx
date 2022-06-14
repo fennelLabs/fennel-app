ef790043c674c555d9eb7335c2722bf03a1691f9
Ed Hertzog <edhertzog@gmail.com>
2022-06-13 21:09:14 -0400

    class to className (#23)

--- src/views/pages/Navigation/TailwindyNav.jsx
+++ src/views/pages/Navigation/TailwindyNav.jsx
@@ -1,52 +1,52 @@
 function TailwindyNav() {
   return (
-    <nav class="relative flex flex-wrap items-center justify-between px-2 py-3 bg-amber-500 mb-3">
-      <div class="container px-4 mx-auto flex flex-wrap items-center justify-between">
-        <div class="w-full relative flex justify-between lg:w-auto  px-4 lg:static lg:block lg:justify-start">
+    <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-amber-500 mb-3">
+      <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+        <div className="w-full relative flex justify-between lg:w-auto  px-4 lg:static lg:block lg:justify-start">
           <a
-            class="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
+            className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
             href="/"
           >
             Our Unnamed App
           </a>
           <button
-            class="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+            className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
             type="button"
           >
-            <span class="block relative w-6 h-px rounded-sm bg-white"></span>
-            <span class="block relative w-6 h-px rounded-sm bg-white mt-1"></span>
-            <span class="block relative w-6 h-px rounded-sm bg-white mt-1"></span>
+            <span className="block relative w-6 h-px rounded-sm bg-white"></span>
+            <span className="block relative w-6 h-px rounded-sm bg-white mt-1"></span>
+            <span className="block relative w-6 h-px rounded-sm bg-white mt-1"></span>
           </button>
         </div>
-        <div class="lg:flex flex-grow items-center" id="example-navbar-warning">
-          <ul class="flex flex-col lg:flex-row list-none ml-auto">
-            <li class="nav-item">
+        <div className="lg:flex flex-grow items-center" id="example-navbar-warning">
+          <ul className="flex flex-col lg:flex-row list-none ml-auto">
+            <li className="nav-item">
               <a
-                class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
                 href="/identity"
               >
                 Manage Identity
               </a>
             </li>
-            <li class="nav-item">
+            <li className="nav-item">
               <a
-                class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
                 href="/contacts"
               >
                 Manage Contacts
               </a>
             </li>
-            <li class="nav-item">
+            <li className="nav-item">
               <a
-                class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
                 href="/inbox"
               >
                 Messaging & Inbox
               </a>
             </li>
-            <li class="nav-item">
+            <li className="nav-item">
               <a
-                class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
                 href="/feed"
               >
                 Feed
