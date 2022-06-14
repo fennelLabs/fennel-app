85d80cba4ad3d69757edd81e86894874dd45c6b9
Husky <huskydev@pm.me>
2022-06-13 20:47:07 -0400

    adding router links

--- src/views/pages/Navigation/TailwindyNav.jsx
+++ src/views/pages/Navigation/TailwindyNav.jsx
@@ -1,3 +1,6 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
 function TailwindyNav() {
   return (
     <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-amber-500 mb-3">
@@ -18,40 +21,27 @@ function TailwindyNav() {
             <span className="block relative w-6 h-px rounded-sm bg-white mt-1"></span>
           </button>
         </div>
-        <div className="lg:flex flex-grow items-center" id="example-navbar-warning">
-          <ul className="flex flex-col lg:flex-row list-none ml-auto">
-            <li className="nav-item">
-              <a
-                className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                href="/identity"
-              >
-                Manage Identity
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                href="/contacts"
-              >
-                Manage Contacts
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                href="/inbox"
-              >
-                Messaging & Inbox
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                href="/feed"
-              >
-                Feed
-              </a>
-            </li>
+        <div class="lg:flex flex-grow items-center" id="example-navbar-warning">
+          <ul class="flex flex-col lg:flex-row list-none ml-auto">
+            {React.Children.toArray(
+              [
+                <Link to="/identity" className="link">
+                  Manage Identity
+                </Link>,
+                <Link to="/contacts" className="link">
+                  Manage Contacts
+                </Link>,
+                <Link to="/identity" className="link">
+                  Manage Identity
+                </Link>,
+                <Link to="/inbox" className="link">
+                  Messaging & Inbox
+                </Link>,
+                <Link to="/feed" className="link">
+                  Feed
+                </Link>
+              ].map((e) => <li>{e}</li>)
+            )}
           </ul>
         </div>
       </div>
