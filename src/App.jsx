b1f9814a0dfb2181322f5db0fad5d392c8a4b0eb
Husky <huskydev@pm.me>
2022-06-15 14:53:57 -0400

    moving routes into its own file

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,11 @@
 import React, {useState, useEffect} from 'react';
-import {BrowserRouter as Router, Route, Routes, Outlet} from 'react-router-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 import './App.css';
-import Navigation from './views/pages/Navigation';
-import Profile from './views/pages/Profile';
-import Inbox from './views/pages/Inbox';
-import Diagnostics from './views/pages/Diagnostics';
-import GenerateKeypair from './views/pages/GenerateKeypair';
-import PublishKey from './views/pages/PublishKey';
-import RevokeKey from './views/pages/RevokeKey';
-import Feed from './views/pages/Feed';
-import Identity from './views/pages/Identity';
-import BackupKey from './views/pages/BackupKey';
-import NewFeedMessage from './views/pages/NewFeedMessage';
-import Contacts from './views/pages/Contacts';
-import AddContact from './views/pages/AddContact';
-import NewMessage from './views/pages/NewMessage';
-import Home from './views/pages/Home';
 import LoadingScreen from './views/components/LoadingScreen';
 import AppContext from './contexts/AppContext';
 import RegisterModal from './addons/Modal/RegisterModal';
-import GenerateWallet from './views/pages/GenerateWallet';
-import RestoreWallet from './views/pages/RestoreWallet';
 import {ServiceContextProvider} from './contexts/ServiceContext';
+import FennelAppRouter from './FennelAppRouter';
 
 function AppLoader({children}) {
   const [loading, setLoading] = useState(true);
@@ -36,37 +20,6 @@ function AppLoader({children}) {
   return children;
 }
 
-function AppRouter() {
-  return (
-    <>
-      <Navigation />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/inbox" element={<Inbox />} />
-        <Route path="/diagnostics" element={<Diagnostics />} />
-        <Route
-          path="/identity/import-generate-keypair"
-          element={<GenerateKeypair />}
-        />
-        <Route path="/identity/profile" element={<Profile />} />
-        <Route path="/identity/publish-key" element={<PublishKey />} />
-        <Route path="/identity/backup-key" element={<BackupKey />} />
-        <Route path="/identity/revoke-key" element={<RevokeKey />} />
-        <Route path="/identity" element={<Identity />} />
-        <Route path="/contacts" element={<Contacts />} />
-        <Route path="/contacts/add" element={<AddContact />} />
-        <Route path="/feed" element={<Feed />} />
-        <Route path="/feed/message" element={<NewFeedMessage />} />
-        <Route path="/inbox/new" element={<NewMessage />} />
-        <Route path="/wallet/generate" element={<GenerateWallet />} />
-        <Route path="/wallet/restore" element={<RestoreWallet />} />
-      </Routes>
-      <Outlet />
-    </>
-  );
-}
-
 function App() {
   const modal = RegisterModal();
 
@@ -79,7 +32,7 @@ function App() {
           }}
         >
           <AppLoader>
-            <AppRouter />
+            <FennelAppRouter />
           </AppLoader>
           {modal.Component}
         </AppContext.Provider>
