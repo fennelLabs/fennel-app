7b704adfa8d2b7368185eacbec18c7ace8b8f714
Husky <huskydev@pm.me>
2022-06-15 13:25:30 -0400

    long-term organization of services

--- src/contexts/ServiceContext.js
+++ src/contexts/ServiceContext.js
@@ -1,5 +1,5 @@
 import {createContext, useContext} from 'react';
-import {MessageService} from '../services/Message.service';
+import {MessageService} from '../services';
 
 const ServiceContext = createContext({});
 
