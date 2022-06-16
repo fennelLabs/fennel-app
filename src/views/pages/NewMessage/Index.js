9fdb0f8a8c0db572675583e5d5fb8c0ef707759c
Ed Hertzog <edhertzog@gmail.com>
2022-06-15 22:58:47 -0400

    Start of message form/functionality

--- src/views/pages/NewMessage/Index.js
+++ src/views/pages/NewMessage/Index.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PageTitle from '../../components/PageTitle';
 import Text from '../../components/Text';
 import InboxSubNav from '../../components/InboxSubNav';
 
 function NewMessage() {
+
+  const [message, setMessage] = useState({
+    message: '',
+    public_key: '',
+    signature: '',
+    fingerprint: '',
+    sender: null,
+    recipient: null,
+    message_encryption_indicator: null
+  });
+
   return (
     <div className="flex flex-row">
       <div className="basis-1/4">
@@ -14,6 +25,17 @@ function NewMessage() {
         <Text>
           Some text explaining what this is all about and what to expect.
         </Text>
+        <form>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Your Message</span>
+            </label>
+            <textarea
+              className="textarea textarea-bordered h-24"
+              placeholder="..."
+            ></textarea>
+          </div>
+        </form>
       </div>
     </div>
   );
