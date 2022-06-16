7a9c535bc3333116875136d63f8e45b4a90d532d
Husky <huskydev@pm.me>
2022-06-15 20:03:53 -0400

    using proper rpc service

--- src/services/rpc.service.js
+++ src/services/rpc.service.js
@@ -184,4 +184,32 @@ export class FennelRPC {
       callback
     );
   }
+
+  whiteflag_encode(callback) {
+    return this.send(
+      {
+        method: 'whiteflag_encode',
+        params: JSON.stringify({
+          prefix: 'WF',
+          version: '1',
+          encryptionIndicator: '0',
+          duressIndicator: '0',
+          messageCode: 'A',
+          referenceIndicator: '0',
+          referencedMessage:
+            '0000000000000000000000000000000000000000000000000000000000000000',
+          verificationMethod: '1',
+          verificationData: 'https://organisation.int/whiteflag'
+        })
+      },
+      callback
+    );
+  }
+
+  whiteflag_decode() {
+    this.send(
+      'whiteflag_decode',
+      '5746313020800000000000000000000000000000000000000000000000000000000000000000b43a3a38399d1797b7b933b0b734b9b0ba34b7b71734b73a17bbb434ba32b33630b380'
+    );
+  }
 }
