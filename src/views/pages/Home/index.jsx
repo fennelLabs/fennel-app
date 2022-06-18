7608529a29f7f6fc5a3484a0f774ee823a1523ed
Ed Hertzog <edhertzog@gmail.com>
2022-06-17 22:30:05 -0400

    Getting started on identity flow

--- src/views/pages/Home/index.jsx
+++ src/views/pages/Home/index.jsx
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import useModal from '../../../utils/useModal';
 import PageTitle from '../../components/PageTitle';
 import Button from '../../components/Button';
+import Redirect from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 function Home() {
+  const [state, setState] = useState({hasFennelAccount: false});
+
+  useEffect(() => {
+    //call to service to determine if they have a fennel account
+    //then set state
+    setState({hasFennelAccount: false});
+  }, []);
+
   const controls = useModal('Menu');
   const okdialog = useModal('OkDialog');
 
-  return (
-    <div className="flex flex-row">
-      <div className="basis-4/4 px-8">
-        <PageTitle>Home</PageTitle>
-        <Button onClick={() => controls.open()}>Open Menu</Button>
-        <Button onClick={() => okdialog.open()}>Ok Dialog</Button>
-      </div>
-    </div>
+  return !state.hasFennelAccount ? (
+    <>
+      <p>
+        A bunch of introductory text explaining that acts as a sort of splash
+        followed by the option to getting started with creating an account.
+      </p>
+      <Link to="/account">Get Started</Link>
+    </>
+  ) : (
+    <Redirect
+      to={{
+        pathname: '/inbox' //redirect to maybe inbox or something else if they have account?
+      }}
+    />
   );
 }
 
