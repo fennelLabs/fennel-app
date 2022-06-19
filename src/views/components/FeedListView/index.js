f89d5d408263401fe52854b1702000943d63ce94
Sean Batzel <romulus108@protonmail.com>
2022-06-19 09:50:33 -0400

    Added a detail view to our FeedListView based on @PhillyWebGuy's message detail view.

--- src/views/components/FeedListView/index.js
+++ src/views/components/FeedListView/index.js
@@ -1,16 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
+import Button from '../Button';
 
 function FeedListView(props) {
-  return (
+  const [state, setState] = useState({readingId: null});
+
+  const toggleItem = (id) => {
+    setState((prevState) => ({
+      ...prevState,
+      readingId: id
+    }));
+  };
+
+  return state.readingId == null ? (
     <div style={{width: '90%'}}>
-      {props.itemList.map((item) => (
-        <div key={item.id}>
-          <div>{item.message}</div>
-          <hr />
-        </div>
-      ))}
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Message</th>
+            </tr>
+          </thead>
+          <tbody>
+            {props.itemList.map((item) => (
+              <tr key={item.id}>
+                <td>
+                  <Button onClick={() => toggleItem(item.id)}>Read</Button>
+                </td>
+                <td>
+                  {item.message.length < 50
+                    ? item.message
+                    : item.message.substr(0, 49) + `...`}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
+  ) : (
+    <>
+      <p className="mb-2">
+        {props.itemList.find((item) => item.id === state.readingId).message}
+      </p>
+      <Button onClick={() => toggleItem(null)}>Close</Button>
+    </>
   );
 }
 
