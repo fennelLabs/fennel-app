7ea5070f9b7f42d7718823e7161760c0493fbe04
Ed Hertzog <edhertzog@gmail.com>
2022-06-20 09:00:42 -0400

    Stub out import keypair page

--- src/views/pages/ImportKeypair/index.js
+++ src/views/pages/ImportKeypair/index.js
@@ -1,9 +1,83 @@
 import React from 'react';
+import IdentitySubNav from '../../components/IdentitySubNav';
+import PageTitle from '../../components/PageTitle';
+import Button from '../../components/Button';
 
 function ImportKeypair() {
+
+  const [state, setState] = useState({useJson: true});
+
+  function handleStateChange(o) {
+    setState((prevState) => ({
+      ...prevState,
+      ...o
+    }));
+  }
+
+  function toggleChoice() {
+    setBtnEnabled(true);
+  }
+
+  handleSubmit = () => {
+    e.preventDefault();
+    if (state.useJson) {
+      console.log('import json');
+      navigate('/'); 
+    } else {
+      console.log('import phrase');
+      navigate('/');
+    }
+  };
+
   return (
-    <div>
-      <h1>Import RSA Keypair</h1>
+    <div className="flex flex-row">
+      <div className="basis-1/4">
+        <IdentitySubNav />
+      </div>
+      <div className="basis-3/4 px-8">
+        <PageTitle>Import Keypair</PageTitle>
+        <form onSubmit={handleSubmit}>
+          <textarea
+            cols="50"
+            className="textarea textarea-bordered"
+            placeholder=""
+            name="keypair"
+          ></textarea>
+          <div className="form-control">
+            <label className="label cursor-pointer">
+              <span className="label-text">
+                <strong>Seed Phrase</strong>
+              </span>
+              <input
+                value="phrase"
+                type="radio"
+                name="radio"
+                className="radio checked:bg-blue-500"
+                onChange={() => handleStateChange({useJson: false})}
+                onClick={() => toggleChoice()}
+              />
+            </label>
+          </div>
+          <div className="form-control">
+            <label className="label cursor-pointer">
+              <span className="label-text">
+                <strong>JSON</strong>
+              </span>
+              <input
+                value="json"
+                type="radio"
+                name="radio"
+                className="radio checked:bg-blue-500"
+                onChange={() => handleStateChange({useJson: true})}
+                onClick={() => toggleChoice()}
+              />
+            </label>
+          </div>
+          <div className="mt-2">
+            <Button type="submit">Import</Button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
