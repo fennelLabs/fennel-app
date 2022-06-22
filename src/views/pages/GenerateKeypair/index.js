10660d472330496984a6a6ffc706da49a1a9e560
Isaac Adams <huskydev@pm.me>
2022-06-22 10:28:12 -0400

    generate keypair fix (#42)

--- src/views/pages/GenerateKeypair/index.js
+++ src/views/pages/GenerateKeypair/index.js
@@ -1,30 +1,15 @@
 import React, {useState} from 'react';
-import {CLI_URI} from '../../../config';
-import {Client as WsClient} from 'rpc-websockets';
 import PageTitle from '../../components/PageTitle';
-import Text from '../../components/Text';
-import Button from '../../components/Button';
 import IdentitySubNav from '../../components/IdentitySubNav';
 import useFennelRPC from '../../hooks/useFennelRPC';
 
 function GenerateKeypair() {
-  const [state, setState] = useState({publicKey: null});
+  const [publicKey, setPublicKey] = useState(null);
   const {rpc} = useFennelRPC();
 
-  function handleStateChange(o) {
-    console.log(o);
-    setState((prevState) => ({
-      ...prevState,
-      ...o
-    }));
-  }
-
-  async function generateKeypair(e) {
-    e.preventDefault();
-    console.log('generate key pair');
-
+  function generateKeypair() {
     rpc.generateKeypair((r) => {
-      handleStateChange({publicKey: r});
+      setPublicKey(r);
     });
   }
 
@@ -35,22 +20,32 @@ function GenerateKeypair() {
       </div>
       <div className="basis-3/4 px-8">
         <PageTitle>Generate Keypair</PageTitle>
-        <Text>{state.publicKey}</Text>
-        {state.publicKey == null ? (
-          <div>
-            <p>
-              You will now create a keypair representing an identity associated
-              with your Fennel account.
-            </p>
-            <form onSubmit={generateKeypair}>
+        <div className="flex flex-col">
+          {!publicKey ? (
+            <>
+              <p>
+                You will now create a keypair representing an identity
+                associated with your Fennel account.
+              </p>
               <div className="mt-2">
-                <Button type="submit">Generate</Button>
+                <button
+                  type="button"
+                  className="btn"
+                  onClick={() => generateKeypair()}
+                >
+                  Generate
+                </button>
               </div>
-            </form>
-          </div>
-        ) : (
-          <span>Success!</span>
-        )}
+            </>
+          ) : (
+            <span className="mb-2">Success!</span>
+          )}
+          {publicKey && (
+            <div className="h-full flex self-center items-center py-3 md:py-0">
+              <div className="break-all max-w-[50rem]">{publicKey}</div>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
