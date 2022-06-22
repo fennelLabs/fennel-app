0d6f0d88c9ac0e9ee6882b37e855997edd194b58
Husky <huskydev@pm.me>
2022-06-22 16:38:37 -0400

    hook for default identity

---
+++ src/views/hooks/useDefaultIdentity.jsx
@@ -0,0 +1,22 @@
+import React, {useState, useEffect} from 'react';
+import {filter} from 'rxjs';
+import {useServiceContext} from '../../contexts/ServiceContext';
+
+export function useDefaultIdentity() {
+  const {node} = useServiceContext();
+  const [identity, setDefaultIdentity] = useState(undefined);
+
+  useEffect(() => {
+    const sub = node?.defaultIdentity$
+      .pipe(filter((i) => !!i))
+      .subscribe((i) => {
+        setDefaultIdentity(i);
+      });
+
+    return () => {
+      sub.unsubscribe();
+    };
+  }, []);
+
+  return identity;
+}
