8b4b71e8e7f423552ecc5cad0f2b8fe5c6379613
Husky <huskydev@pm.me>
2022-06-22 16:10:42 -0400

    registering node service

--- src/contexts/ServiceContext.js
+++ src/contexts/ServiceContext.js
@@ -3,11 +3,12 @@ import {createContext, useContext} from 'react';
 import {FennelRPC} from '../services';
 import MessageAPIService from '../services/MessageAPI';
 import KeyManager from '../services/KeyManager';
+import Node from '../services/Node';
 
 const ServiceContext = createContext({});
 
 /**
- * @returns {{messageService: MessageAPIService, rpc: FennelRPC, keymanager: KeyManager}}
+ * @returns {{messageService: MessageAPIService, rpc: FennelRPC, keymanager: KeyManager, node: Node}}
  */
 export const useServiceContext = () => useContext(ServiceContext);
 
@@ -16,11 +17,12 @@ export const ServiceContextProvider = ({children}) => {
 
   useEffect(() => {
     // all services get instantiated once and registered into the context provider
+    const node = new Node();
     const rpc = new FennelRPC();
     const messageService = new MessageAPIService(rpc);
     const keymanager = new KeyManager('Main');
 
-    setServices({messageService, rpc, keymanager});
+    setServices({messageService, rpc, keymanager, node});
 
     return () => {
       setServices(undefined);
