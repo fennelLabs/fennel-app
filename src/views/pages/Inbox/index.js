051b3ac02653ef4d7f3a949d7f11c5a374830840
Husky <huskydev@pm.me>
2022-06-22 11:23:43 -0400

    split up the logic into two use effect hooks since the subscription and interval should only run once while the check messages should run on every entracne

--- src/views/pages/Inbox/index.js
+++ src/views/pages/Inbox/index.js
@@ -1,55 +1,57 @@
 import React, {useEffect, useState} from 'react';
-import MessageAPIService from '../../../services/MessageAPI';
 import ListView from '../../components/ListView';
 import PropTypes from 'prop-types';
 import PageTitle from '../../components/PageTitle';
 import InboxSubNav from '../../components/InboxSubNav';
+import {useServiceContext} from '../../../contexts/ServiceContext';
 
-const message = new MessageAPIService();
+const test_messages = [
+  {
+    id: 0,
+    message: '000 dolor lorum ipsum dolor lorum ipsum',
+    fingerprint: 'asdfas99234js9siw2923jsakfaskasd92'
+  },
+  {
+    id: 1,
+    message:
+      '111 dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum',
+    fingerprint: 'asdfas99234js9siw2923jsakfaskasd92'
+  },
+  {
+    id: 2,
+    message:
+      '222 dolor lorum ipsum dolor lorum ipsum  dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum',
+    fingerprint: 'asdfas99234js9siw2923jsakfaskasd92'
+  },
+  {
+    id: 3,
+    message:
+      '333 dolor lorum ipsum dolor lorum ipsum  dolor lorum ipsum dolor lorum ipsum  dolor lorum ipsum dolor lorum ipsum',
+    fingerprint: 'asdfas99234js9siw2923jsakfaskasd92'
+  }
+];
 
 function Inbox(props) {
-  const [messageList, setMessageList] = useState([]);
+  const {messageService} = useServiceContext();
+  const [messageList, setMessageList] = useState(null);
 
   useEffect(() => {
-    const sub = message.received_messages$.subscribe((d) => {
-      let testList = [
-        {
-          id: 0,
-          message: '000 dolor lorum ipsum dolor lorum ipsum',
-          fingerprint: 'asdfas99234js9siw2923jsakfaskasd92'
-        },
-        {
-          id: 1,
-          message:
-            '111 dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum',
-          fingerprint: 'asdfas99234js9siw2923jsakfaskasd92'
-        },
-        {
-          id: 2,
-          message:
-            '222 dolor lorum ipsum dolor lorum ipsum  dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum dolor lorum ipsum',
-          fingerprint: 'asdfas99234js9siw2923jsakfaskasd92'
-        },
-        {
-          id: 3,
-          message:
-            '333 dolor lorum ipsum dolor lorum ipsum  dolor lorum ipsum dolor lorum ipsum  dolor lorum ipsum dolor lorum ipsum',
-          fingerprint: 'asdfas99234js9siw2923jsakfaskasd92'
-        }
-      ];
-      setMessageList(testList);
+    const sub = messageService.received_messages$.subscribe((d) => {
+      setMessageList(d && d.length > 0 ? d : null);
     });
 
-    message.checkMessages(props.user_identity);
-
     let id = setInterval(() => {
-      message.checkMessages(props.user_identity);
+      messageService.checkMessages(props.user_identity);
     }, 5000);
 
     return () => {
       sub.remove();
       clearInterval(id);
     };
+  }, []);
+
+  useEffect(() => {
+    messageService.checkMessages(props.user_identity);
   }, [props.user_identity]);
 
   return (
@@ -59,6 +61,11 @@ function Inbox(props) {
       </div>
       <div className="basis-3/4 px-8">
         <PageTitle>Inbox</PageTitle>
+        {!messageList && (
+          <button className="btn" onClick={() => setMessageList(test_messages)}>
+            Load Test Messages
+          </button>
+        )}
         <ListView itemList={messageList ?? []} />
       </div>
     </div>
