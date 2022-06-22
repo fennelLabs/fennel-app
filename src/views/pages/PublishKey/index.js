4d42242b5d7eb17b26de0244a86f45fbc553f719
Isaac Adams <huskydev@pm.me>
2022-06-22 12:39:49 -0400

    fixing up UI and a few service layer errors (#43)

--- src/views/pages/PublishKey/index.js
+++ src/views/pages/PublishKey/index.js
@@ -5,31 +5,33 @@ import Button from '../../components/Button';
 import IdentitySubNav from '../../components/IdentitySubNav';
 import Node from '../../../services/Node';
 import ContactsManager from '../../../services/ContactsManager.service';
+import {useServiceContext} from '../../../contexts/ServiceContext';
 
 const node = new Node();
 const contactsManager = new ContactsManager();
 
 function PublishKey() {
+  const {keymanager} = useServiceContext();
   const [success, setSuccess] = useState(false);
   const [fingerprint, setFingerprint] = useState('');
   const [location, setLocation] = useState('');
 
   async function publishKey(e) {
     e.preventDefault();
-    let result = await node.announceKey(fingerprint, location);
+    let result = await node.announceKey(keymanager, fingerprint, location);
     if (result) {
-      await contactsManager.createNewIdentity(fingerprint, publishKey);
+      await contactsManager.createNewIdentity(fingerprint, result);
     }
     setSuccess(result);
   }
 
   function handleFingerprintChange(e) {
-    const {_, value} = e.target;
+    const {value} = e.target;
     setFingerprint(value);
   }
 
   function handleLocationChange(e) {
-    const {_, value} = e.target;
+    const {value} = e.target;
     setLocation(value);
   }
 
@@ -47,17 +49,68 @@ function PublishKey() {
         ) : (
           <Text>Keypair published successfully.</Text>
         )}
-        <form onSubmit={publishKey}>
-          <textarea
-            value={fingerprint}
-            onChange={handleFingerprintChange}
-            className="textarea textarea-bordered h-24"
-          ></textarea>
-          <textarea
-            value={location}
-            onChange={handleLocationChange}
-            className="textarea textarea-bordered h-24"
-          ></textarea>
+        <form className="grid" onSubmit={publishKey}>
+          <div className="form-group mb-6">
+            <label
+              htmlFor="name"
+              className="form-label inline-block mb-2 text-gray-700"
+            >
+              Name
+            </label>
+            <input
+              id="name"
+              value={location}
+              onChange={handleLocationChange}
+              type="text"
+              className="
+                form-control-bordered
+                block
+                w-full
+                px-3
+                py-1.5
+                text-base
+                font-normal
+                text-gray-700
+                bg-white bg-clip-padding
+                border border-solid border-gray-300
+                rounded
+                transition
+                ease-in-out
+                m-0
+                focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+            />
+          </div>
+          <div className="form-group mb-6">
+            <label
+              htmlFor="fingerprint"
+              className="form-label inline-block mb-2 text-gray-700"
+            >
+              Fingerprint
+            </label>
+            <textarea
+              id="fingerprint"
+              value={fingerprint}
+              onChange={handleFingerprintChange}
+              //className=" h-24"
+              className="
+                textarea-bordered
+                block
+                w-full
+                px-3
+                py-1.5
+                text-base
+                font-normal
+                text-gray-700
+                bg-white bg-clip-padding
+                border border-solid border-gray-300
+                rounded
+                transition
+                ease-in-out
+                m-0
+                focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+            ></textarea>
+          </div>
+
           <div className="mt-2">
             <Button type="submit">Publish Key</Button>
           </div>
