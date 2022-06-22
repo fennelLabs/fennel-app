4efc94c85707f51d08a377b5a3db50b97382c718
Isaac Adams <huskydev@pm.me>
2022-06-22 14:27:41 -0400

    adding error and success feedback for restore wallet (#48)
    Co-authored-by: Sean Batzel <romulus108@protonmail.com>

--- src/views/pages/RestoreWallet/index.js
+++ src/views/pages/RestoreWallet/index.js
@@ -1,35 +1,66 @@
 import React, {useState} from 'react';
 import {useServiceContext} from '../../../contexts/ServiceContext';
+import {Error} from './Error';
 
 function RestoreWallet(props) {
-  const [value, setValue] = useState('');
+  const [seedPhrase, setSeedPhrase] = useState('');
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(null);
   const {keymanager} = useServiceContext();
 
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
-        keymanager.importAccount('Main', value);
+        if (
+          validateSeedPhrase() &&
+          keymanager.importAccount('Main', seedPhrase)
+        ) {
+          setSuccess(true);
+          setError(null);
+        }
       }}
       className="grid gap-4 w-full h-full"
     >
       <h1>Import Fennel Wallet</h1>
+      <div className="flex flex-row">
+        <div>
+          <button className="btn" type="submit">
+            Submit
+          </button>
+        </div>
+        <div className="ml-4 self-center px-3">
+          <span>
+            {success && 'Successfully imported wallet!'}
+            <Error>{error}</Error>
+          </span>
+        </div>
+      </div>
       <textarea
         name="new_message"
         rows={5}
         cols={5}
-        value={value}
+        value={seedPhrase}
         onChange={(event) => {
-          setValue(event.target.value);
+          setSeedPhrase(event.target.value);
         }}
       />
-      <div className="my-2">
-        <button className="btn" type="submit">
-          Submit
-        </button>
-      </div>
     </form>
   );
+
+  function validateSeedPhrase() {
+    setSuccess(false);
+    const isValid =
+      seedPhrase && seedPhrase.length > 0 && !/^\s+$/g.test(seedPhrase);
+
+    if (isValid && error) {
+      setError(null);
+    } else if (!isValid && !error) {
+      setError('not a valid seed phrase');
+    }
+
+    return isValid;
+  }
 }
 
 export default RestoreWallet;
