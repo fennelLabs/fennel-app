b5f3597fd152b781f4841564d2644d2129a2bd30
Ed Hertzog <edhertzog@gmail.com>
2022-06-21 20:44:52 -0400

    remove unused/unneeded

--- src/views/pages/ImportKeypair/index.js
+++ src/views/pages/ImportKeypair/index.js
@@ -6,36 +6,16 @@ import {useNavigate} from 'react-router-dom';
 import {useServiceContext} from '../../../contexts/ServiceContext';
 
 function ImportKeypair() {
-  const [state, setState] = useState({useJson: true, isComplete: false});
-  const [btnEnabled, setBtnEnabled] = useState(false);
   const navigate = useNavigate();
   const [mnemonic, setMnemonic] = useState('');
+  const [isComplete, setIsComplete] = useState(false);
   const [key, setKey] = useState('');
   const {_, keymanager} = useServiceContext();
 
-  function handleStateChange(o) {
-    setState((prevState) => ({
-      ...prevState,
-      ...o
-    }));
-  }
-
-  function toggleChoice() {
-    setBtnEnabled(true);
-  }
-
   handleSubmit = (e) => {
     e.preventDefault();
-    handleStateChange({isComplete: true});
-    if (state.useJson) {
-      console.log('import json');
-      setMnemonic(keymanager.importAccount('Main', mnemonic));
-      //navigate('/');
-    } else {
-      console.log('import phrase');
-      setMnemonic(keymanager.importAccount('Main', mnemonic));
-      //navigate('/');
-    }
+    setMnemonic(keymanager.importAccount('Main', key));
+    setIsComplete(true);
   };
 
   return (
@@ -45,7 +25,7 @@ function ImportKeypair() {
       </div>
       <div className="basis-3/4 px-8">
         <PageTitle>Import Account</PageTitle>
-        {!state.isComplete ? (
+        {!isComplete ? (
           <form onSubmit={handleSubmit}>
             <textarea
               cols="50"
@@ -54,38 +34,8 @@ function ImportKeypair() {
               name="keypair"
               onChange={(event) => setKey(event.target.value)}
             ></textarea>
-            <div className="form-control">
-              <label className="label cursor-pointer">
-                <span className="label-text">
-                  <strong>Seed Phrase</strong>
-                </span>
-                <input
-                  value="phrase"
-                  type="radio"
-                  name="radio"
-                  className="radio checked:bg-blue-500"
-                  onChange={() => handleStateChange({useJson: false})}
-                  onClick={() => toggleChoice()}
-                />
-              </label>
-            </div>
-            <div className="form-control">
-              <label className="label cursor-pointer">
-                <span className="label-text">
-                  <strong>JSON</strong>
-                </span>
-                <input
-                  value="json"
-                  type="radio"
-                  name="radio"
-                  className="radio checked:bg-blue-500"
-                  onChange={() => handleStateChange({useJson: true})}
-                  onClick={() => toggleChoice()}
-                />
-              </label>
-            </div>
             <div className="mt-2">
-              {btnEnabled && <Button type="submit">Import</Button>}
+              {key && <Button type="submit">Import</Button>}
             </div>
           </form>
         ) : (
