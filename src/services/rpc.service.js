d54dcefdad48d86a911932456e02c6fd8588f049
Ed Hertzog <edhertzog@gmail.com>
2022-07-01 13:44:14 -0400

    throw error

--- src/services/rpc.service.js
+++ src/services/rpc.service.js
@@ -1,4 +1,4 @@
-import {BehaviorSubject, filter, map, ReplaySubject, throwError} from 'rxjs';
+import {BehaviorSubject, filter, map, ReplaySubject} from 'rxjs';
 import {CLI_URI} from '../config';
 
 export class FennelRPC {
