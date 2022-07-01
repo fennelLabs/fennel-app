513d043c18c7241e4b21d037585974ad45f22713
Ed Hertzog <edhertzog@gmail.com>
2022-07-01 12:25:02 -0400

    First pass at error pattern

--- src/views/pages/GenerateWallet/index.js
+++ src/views/pages/GenerateWallet/index.js
@@ -1,19 +1,31 @@
 import React, {useState, useContext} from 'react';
 import {useServiceContext} from '../../../contexts/ServiceContext';
 import Button from '../../components/Button';
+import Error from '../../components/Error';
 import Text from '../../components/Text';
 
 function GenerateWallet() {
   const [mnemonic, setMnemonic] = useState('');
   const {_, keymanager} = useServiceContext();
+  const [error, setError] = useState('');
+
+  function generateMnemonic() {
+    try {
+      setMnemonic(keymanager.generateAccount('Main'));
+      setError('');
+    } catch {
+      setError('Key Manager has failed. Please try again.');
+    }
+  }
 
   return (
     <div>
       <h1>Generate Wallet</h1>
       <Text>{mnemonic}</Text>
+      {error && <Error>{error}</Error>}
       <Button
         onClick={() => {
-          setMnemonic(keymanager.generateAccount('Main'));
+          generateMnemonic();
         }}
       >
         Generate
