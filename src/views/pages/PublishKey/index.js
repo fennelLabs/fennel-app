fb92f97303f8c9b49f91c89b794dd22bac553510
Ed Hertzog <edhertzog@gmail.com>
2022-07-01 14:12:28 -0400

    Error handling for publish key

--- src/views/pages/PublishKey/index.js
+++ src/views/pages/PublishKey/index.js
@@ -10,22 +10,31 @@ function PublishKey() {
   const {node, keymanager, contactsManager} = useServiceContext();
   const defaultIdentity = useDefaultIdentity();
   const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(undefined);
 
   const [fingerprint, location, PublishKeyForm] = usePublishKeyForm({
     onSubmit: publishKey
   });
 
   async function publishKey() {
-    let result = await node.announceKey(keymanager, fingerprint, location);
-    if (result && defaultIdentity) {
-      console.log('Creating a new message server identity.');
-      await contactsManager.createNewIdentity(
-        defaultIdentity,
-        fingerprint,
-        location
+    try {
+      console.log('Start publishKey()');
+      let result = await node.announceKey(keymanager, fingerprint, location);
+      if (result && defaultIdentity) {
+        console.log('Creating a new message server identity.');
+        await contactsManager.createNewIdentity(
+          defaultIdentity,
+          fingerprint,
+          location
+        );
+      }
+      setSuccess(result);
+      setError(undefined);
+    } catch {
+      setError(
+        'Publishing your key has failed. This may be a temporary problem. If refreshing this page does not result in success, please contact:'
       );
     }
-    setSuccess(result);
   }
 
   return (
@@ -35,14 +44,16 @@ function PublishKey() {
       </div>
       <div className="basis-3/4 px-8">
         <PageTitle>Publish Key</PageTitle>
-        {success ? (
-          <Text>
-            Some text explaining what this is all about and what to expect.
-          </Text>
-        ) : (
-          <Text>Keypair published successfully.</Text>
-        )}
+        {success && <Text>Keypair published successfully.</Text>}
         {defaultIdentity && PublishKeyForm}
+        {error && (
+          <div
+            className="bg-red-100 rounded-lg py-5 px-6 mb-4 text-base text-red-700 mb-3"
+            role="alert"
+          >
+            {error}
+          </div>
+        )}
       </div>
     </div>
   );
