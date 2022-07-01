6e5a66149d3d1157d24a4dcbdfea5c9ba32e6c4b
Ed Hertzog <edhertzog@gmail.com>
2022-07-01 13:30:34 -0400

    Change of error message text.

--- src/views/pages/GenerateWallet/index.js
+++ src/views/pages/GenerateWallet/index.js
@@ -1,18 +1,17 @@
 import React, {useState, useContext} from 'react';
 import {useServiceContext} from '../../../contexts/ServiceContext';
 import Button from '../../components/Button';
-import Error from '../../components/Error';
 import Text from '../../components/Text';
 
 function GenerateWallet() {
   const [mnemonic, setMnemonic] = useState('');
-  const {_, keymanager} = useServiceContext();
-  const [error, setError] = useState('');
+  const {keymanager} = useServiceContext();
+  const [error, setError] = useState(undefined);
 
   function generateMnemonic() {
     try {
       setMnemonic(keymanager.generateAccount('Main'));
-      setError('');
+      setError(undefined);
     } catch {
       setError('Key Manager has failed. Please try again.');
     }
@@ -22,7 +21,14 @@ function GenerateWallet() {
     <div>
       <h1>Generate Wallet</h1>
       <Text>{mnemonic}</Text>
-      {error && <Error>{error}</Error>}
+      {error && (
+        <div
+          className="bg-red-100 rounded-lg py-5 px-6 mb-4 text-base text-red-700 mb-3"
+          role="alert"
+        >
+          {error}
+        </div>
+      )}
       <Button
         onClick={() => {
           generateMnemonic();
