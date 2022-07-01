d61533969c69bfb061ec6e1a4967003d65563485
Sean Batzel <romulus108@protonmail.com>
2022-07-01 18:11:57 -0400

    Started on a key revocation integration.

--- src/views/pages/RevokeKey/index.js
+++ src/views/pages/RevokeKey/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import PageTitle from '../../components/PageTitle';
 import Text from '../../components/Text';
-import Button from '../../components/Button';
 import IdentitySubNav from '../../components/IdentitySubNav';
+import {useServiceContext} from '../../../contexts/ServiceContext';
+import {useDefaultIdentity} from '../../hooks/useDefaultIdentity';
 
 function RevokeKey() {
+  const [fingerprint, setFingerprint] = useState('');
+  const {node} = useServiceContext();
+  const [success, setSuccess] = useState(false);
+  const defaultIdentity = useDefaultIdentity();
+
+  function handleFingerprintChange(e) {
+    const {value} = e.target;
+    setFingerprint(value);
+  }
+
+  async function revokeKey() {
+    let result = await node.revokeKey(fingerprint);
+    if (result && defaultIdentity) {
+      // Need a contactsManager function to delete contacts.
+    }
+    setSuccess(result);
+  }
+
   return (
     <div className="flex flex-row">
       <div className="basis-1/4">
@@ -15,7 +34,23 @@ function RevokeKey() {
         <Text>
           Some text explaining what this is all about and what to expect.
         </Text>
-        <Button>Revoke Key</Button>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            revokeKey();
+          }}
+        >
+          <div>
+            <textarea
+              id="fingerprint"
+              value={fingerprint}
+              onChange={handleFingerprintChange}
+            ></textarea>
+          </div>
+          <button type="submit" className="btn">
+            Revoke Key
+          </button>
+        </form>
       </div>
     </div>
   );
