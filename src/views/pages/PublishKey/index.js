51fdfbcd55d12b8a53a7094d845f8251f21173ec
Ed Hertzog <edhertzog@gmail.com>
2022-07-01 21:34:15 -0400

    Key missing msg

--- src/views/pages/PublishKey/index.js
+++ src/views/pages/PublishKey/index.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import PageTitle from '../../components/PageTitle';
 import Text from '../../components/Text';
+import {Link} from 'react-router-dom';
 import IdentitySubNav from '../../components/IdentitySubNav';
 import {useServiceContext} from '../../../contexts/ServiceContext';
 import {useDefaultIdentity} from '../../hooks/useDefaultIdentity';
@@ -12,16 +13,16 @@ function PublishKey() {
   const [success, setSuccess] = useState(false);
   const [error, setError] = useState(undefined);
 
+  console.log(defaultIdentity);
+
   const [fingerprint, location, PublishKeyForm] = usePublishKeyForm({
     onSubmit: publishKey
   });
 
   async function publishKey() {
     try {
-      console.log('Start publishKey()');
       let result = await node.announceKey(keymanager, fingerprint, location);
       if (result && defaultIdentity) {
-        console.log('Creating a new message server identity.');
         await contactsManager.createNewIdentity(
           defaultIdentity,
           fingerprint,
@@ -46,6 +47,11 @@ function PublishKey() {
         <PageTitle>Publish Key</PageTitle>
         {success && <Text>Keypair published successfully.</Text>}
         {defaultIdentity && PublishKeyForm}
+        {defaultIdentity == undefined && (
+          <Link to="/identity">
+            Please create an identity before publishing you key.
+          </Link>
+        )}
         {error && (
           <div
             className="bg-red-100 rounded-lg py-5 px-6 mb-4 text-base text-red-700 mb-3"
