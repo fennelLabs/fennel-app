d31c3558c671ad5b1af983fd1a2483bc1ccc3280
Ed Hertzog <edhertzog@gmail.com>
2022-07-02 17:54:18 -0400

    Error handling (#53)
    * First pass at error pattern
    
    * Change of error message text.
    
    * Generate keypair error.
    
    * throw error
    
    * Error handling for publish key
    
    * Key missing msg
    
    * recommended changes
    
    * Added .error
    
    Co-authored-by: Sean Batzel <romulus108@protonmail.com>

--- src/views/pages/GenerateWallet/index.js
+++ src/views/pages/GenerateWallet/index.js
@@ -5,15 +5,33 @@ import Text from '../../components/Text';
 
 function GenerateWallet() {
   const [mnemonic, setMnemonic] = useState('');
-  const {_, keymanager} = useServiceContext();
+  const {keymanager} = useServiceContext();
+  const [error, setError] = useState(undefined);
+
+  function generateMnemonic() {
+    try {
+      setMnemonic(keymanager.generateAccount('Main'));
+      setError(undefined);
+    } catch {
+      setError('Key Manager has failed. Please try again.');
+    }
+  }
 
   return (
     <div>
       <h1>Generate Wallet</h1>
       <Text>{mnemonic}</Text>
+      {error && (
+        <div
+          className="bg-red-100 rounded-lg py-5 px-6 mb-4 text-base text-red-700 mb-3"
+          role="alert"
+        >
+          {error}
+        </div>
+      )}
       <Button
         onClick={() => {
-          setMnemonic(keymanager.generateAccount('Main'));
+          generateMnemonic();
         }}
       >
         Generate
