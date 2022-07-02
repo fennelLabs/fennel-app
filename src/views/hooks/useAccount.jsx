ef6cb9e80779fd77cf69305b50c6b7ec6d3072b4
Husky <huskydev@pm.me>
2022-07-02 00:05:59 -0400

    adding hook for components to react to account being loaded or not

---
+++ src/views/hooks/useAccount.jsx
@@ -0,0 +1,20 @@
+import React, {useState, useEffect} from 'react';
+import {filter} from 'rxjs';
+import {useServiceContext} from '../../contexts/ServiceContext';
+
+export function useAccount() {
+  const {keymanager} = useServiceContext();
+  const [account, setAccount] = useState(undefined);
+
+  useEffect(() => {
+    const sub = keymanager?.pair$.pipe(filter((a) => !!a)).subscribe((a) => {
+      setAccount(a);
+    });
+
+    return () => {
+      sub.unsubscribe();
+    };
+  }, []);
+
+  return account;
+}
--- src/views/pages/Identity/index.jsx
+++ src/views/pages/Identity/index.jsx
@@ -2,8 +2,11 @@ import React, {useState, useContext} from 'react';
 import PageTitle from '../../components/PageTitle';
 import IdentitySubNav from '../../components/IdentitySubNav';
 import {IdentityControls} from './IdentityControls';
+import {useAccount} from '../../hooks/useAccount';
 
 function Identity() {
+  const account = useAccount();
+
   return (
     <div className="flex flex-row">
       <div className="basis-1/4">
@@ -11,7 +14,7 @@ function Identity() {
       </div>
       <div className="basis-3/4 px-8">
         <PageTitle>Identity</PageTitle>
-        <IdentityControls />
+        {account && <IdentityControls />}
       </div>
     </div>
   );
