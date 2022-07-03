f03995258faa049b989110d4b00c3db586074a1a
Sean Batzel <romulus108@protonmail.com>
2022-07-03 14:35:40 -0400

    TransactionConfirm will now take a function allowing state tie-in to the parent component.

--- src/addons/Modal/TransactionConfirm/index.jsx
+++ src/addons/Modal/TransactionConfirm/index.jsx
@@ -1,8 +1,9 @@
 import React, {useContext} from 'react';
 import {ModalContext} from '../useModalContext';
 import ModalTemplate from '../Template';
+import PropTypes from 'prop-types';
 
-function TransactionConfirm() {
+function TransactionConfirm(props) {
   const {controls} = useContext(ModalContext);
   return (
     <ModalTemplate
@@ -13,10 +14,21 @@ function TransactionConfirm() {
       }}
     >
       This action will incur a Fennel Token fee.
-      <button onClick={() => controls.close()}>Confirm</button>
+      <button
+        onClick={() => {
+          props.onConfirm();
+          controls.close();
+        }}
+      >
+        Confirm
+      </button>
       <button onClick={() => controls.close()}>Cancel</button>
     </ModalTemplate>
   );
 }
+
+TransactionConfirm.propTypes = {
+  onConfirm: PropTypes.function
+};
 
 export default TransactionConfirm;
