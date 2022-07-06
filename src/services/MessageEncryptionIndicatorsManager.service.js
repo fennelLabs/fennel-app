e7d7be191c9170c2953ed90d9d8e47f9a2ff83ff
Isaac Adams <huskydev@pm.me>
2022-07-06 15:19:00 -0400

    incorrect axios syntax was causing the encryption modes to not load properly (#63)

--- src/services/MessageEncryptionIndicatorsManager.service.js
+++ src/services/MessageEncryptionIndicatorsManager.service.js
@@ -7,21 +7,21 @@ export default class MessageEncryptionIndicatorsManager {
   message_encryption_indicators$ =
     this._message_encryption_indicators.asObservable();
 
-  async populateIndicators() {
-    let results = await axios
-      .get(`http://localhost:1234/api/message_encryption_indicators/`, {
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
-      .then(function (response) {
-        console.log(response.data.results);
-        return response.data.results;
+  populateIndicators() {
+    axios({
+      method: 'get',
+      url: 'http://localhost:1234/api/message_encryption_indicators/',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((response) => {
+        console.log(response);
+        this._message_encryption_indicators.next([...response.data]);
       })
       .catch(function (error) {
         console.error(error);
         return [];
       });
-    this._message_encryption_indicators.next([...results]);
   }
 }
