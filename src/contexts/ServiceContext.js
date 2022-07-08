c0ae2841561ed3aa072b715a1281fe2be4d20c95
Isaac Adams <huskydev@pm.me>
2022-07-08 18:28:55 -0400

    instantiating all services outside react (#65)

--- src/contexts/ServiceContext.js
+++ src/contexts/ServiceContext.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React from 'react';
 import {createContext, useContext} from 'react';
 import PropTypes from 'prop-types';
 import {FennelRPC} from '../services';
@@ -9,7 +9,23 @@ import ContactsManager from '../services/ContactsManager.service';
 import {ApiPromise} from '@polkadot/api';
 import connect from '../utils/loadPolkadotApi';
 
-const ServiceContext = createContext({});
+const polkadotApi = connect();
+const rpc = new FennelRPC();
+const messageService = new MessageAPIService(rpc);
+const keymanager = new KeyManager('Main');
+const contactsManager = new ContactsManager();
+const node = new Node(polkadotApi);
+
+const services = {
+  polkadotApi,
+  rpc,
+  messageService,
+  keymanager,
+  contactsManager,
+  node
+};
+
+const ServiceContext = createContext(services);
 
 /**
  * <description>
@@ -28,24 +44,8 @@ const ServiceContext = createContext({});
 export const useServiceContext = () => useContext(ServiceContext);
 
 export const ServiceContextProvider = ({children}) => {
-  const polkadotApi = useRef(connect());
-  const rpc = useRef(new FennelRPC());
-  const messageService = useRef(new MessageAPIService(rpc));
-  const keymanager = useRef(new KeyManager('Main'));
-  const contactsManager = useRef(new ContactsManager());
-  const node = useRef(new Node(polkadotApi.current));
-
   return (
-    <ServiceContext.Provider
-      value={{
-        polkadotApi: polkadotApi.current,
-        messageService: messageService.current,
-        rpc: rpc.current,
-        keymanager: keymanager.current,
-        node: node.current,
-        contactsManager: contactsManager.current
-      }}
-    >
+    <ServiceContext.Provider value={services}>
       {children}
     </ServiceContext.Provider>
   );
