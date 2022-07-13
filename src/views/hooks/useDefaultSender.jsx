b8d261cbf3e79544c744abcd2a597acf1862a555
Sean Batzel <romulus108@protonmail.com>
2022-07-13 12:39:52 -0400

    More description of what was going wrong.

--- src/views/hooks/useDefaultSender.jsx
+++ src/views/hooks/useDefaultSender.jsx
@@ -7,9 +7,16 @@ export function useDefaultSender() {
   const [sender, setDefaultSender] = useState(undefined);
 
   useEffect(() => {
+    // It all dropped into place after Isaac's review.
+    // The problem wasn't that defaultSender isn't
+    // being returned correctly, it's that in the case where
+    // this error was occurring it genuinely was undefined.
     const sub = contactsManager?.defaultSender$
-      //.pipe(filter((i) => !!i)) This line never let the value through.
+    // I understand fully why these null checks are useful and why the pipe matters and what it does, the issue is that
+    // if this next line _doesn't_ catch anything
+      .pipe(filter((i) => !!i)) // (This line comes back because I misremembered what was causing the problem.)
       .subscribe((i) => {
+        // This never fires, and if you
         setDefaultSender(i);
       });
 
@@ -17,6 +24,8 @@ export function useDefaultSender() {
       sub.unsubscribe();
     };
   }, []);
+
+  // console.log(sender) here when that subscribe never fired you'd get back `undefined` which is what caused the bug in the first place.
 
   return sender;
 }
