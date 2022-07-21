70d52238f032fbee53e83923693d3a931011df03
Sean Batzel <romulus108@protonmail.com>
2022-07-21 10:50:37 -0400

    Built out a basic wrapper over the IPFS core API.

--- src/services/ipfs.service.js
+++ src/services/ipfs.service.js
@@ -1,7 +1,38 @@
 export default class IPFS {
-  getFile() {}
+  constructor(ipfs) {
+    this._ipfs = ipfs;
+    this._encoder = new TextEncoder();
+    this._decoder = new TextDecoder();
+  }
 
-  putFile() {}
+  // Given an IPFS CID, returns the material stored at that CID.
+  async getFile(cid) {
+    const block = await ipfs.block.get(cid, {timeout: 3000});
+    return this._decoder.decode(block);
+  }
 
-  delFile() {}
+  // Given file content, encodes, submits, verifies, and returns the CID at which the argument is stored.
+  async putFile(content) {
+    const encoded_content = this._encoder(content);
+    const cid = await ipfs.block.put(encoded_content, {
+      timeout: 3000,
+      pin: true
+    });
+    return cid;
+  }
+
+  // Given a CID, tries to delete the CID and returns whether the operation succeeded or not.
+  // Check the Javascript console to determine the reason for negative returns.
+  async delFile(cid) {
+    for await (const result of ipfs.block.rm(cid, {timeout: 3000})) {
+      if (result.error) {
+        console.error(
+          `Failed to remove block ${result.cid} due to ${result.error.message}`
+        );
+        return false;
+      } else {
+        return true;
+      }
+    }
+  }
 }
