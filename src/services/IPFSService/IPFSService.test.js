b4f7d83f13ac807c75f2b3086759923c48b0b143
Ed Hertzog <edhertzog@gmail.com>
2022-07-29 23:24:58 -0400

    Added basic shallow unit test of component, fixed jest jsx error

--- src/services/IPFSService/IPFSService.test.js
+++ src/services/IPFSService/IPFSService.test.js
@@ -1,14 +1,23 @@
-import useIpfsFactory from "../../views/hooks/useIpfsFactory";
+import useIpfsFactory from '../../views/hooks/useIpfsFactory';
+
+/*describe('ipfsFactory', () => {
+  it('should...', () => {
+    const {ipfsService} = useIpfsFactory();
+    const testfile = ipfsService.putFile('Test');
+    const testfile_content = ipfsService.getFile(testfile);
+    expect(testfile_content).toBe('Test');
+  });
+});*/
 
 test('getting a file gets correct content back', () => {
-    const {ipfsService} = useIpfsFactory();
-    const testfile = ipfsService.putFile("Test");
-    const testfile_content = ipfsService.getFile(testfile);
-    expect(testfile_content).toBe("Test");
+  const {ipfsService} = useIpfsFactory();
+  const testfile = ipfsService.putFile('Test');
+  const testfile_content = ipfsService.getFile(testfile);
+  expect(testfile_content).toBe('Test');
 });
 
 test('deleting a file succeeds', () => {
-    const {ipfsService} = useIpfsFactory();
-    const testfile = ipfsService.putFile("Test");
-    expect(ipfsService.delFile(testfile)).toBeTruthy();
+  const {ipfsService} = useIpfsFactory();
+  const testfile = ipfsService.putFile('Test');
+  expect(ipfsService.delFile(testfile)).toBeTruthy();
 });
