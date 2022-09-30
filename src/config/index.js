6670a5786806a34e0844e86af32f136f994b215b
Sean Batzel <romulus108@protonmail.com>
2022-09-29 23:49:46 -0400

    Switching over to an .env file.

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
-export const NODE_URI_WS = 'ws://127.0.0.1:9944';
-export const NODE_URI_HTTP = 'http://127.0.0.1:9933';
-export const CLI_URI = 'ws://localhost:9030';
-export const API_MESSAGES = 'http://localhost:1234/api/messages';
-export const API_IDENTITIES = 'http://localhost:1234/api/identities';
+export const NODE_URI_WS = `ws://${process.env.PROTOCOL_HOST}:9944`;
+export const NODE_URI_HTTP = `http://${process.env.PROTOCOL_HOST}:9933`;
+export const CLI_URI = `ws://${process.env.RPC_HOST}:9030`;
+export const API_MESSAGES = `http://${process.env.API_HOST}:1234/api/messages`;
+export const API_IDENTITIES = `http://${process.env.API_HOST}:1234/api/identities`;
 export const API_MESSAGE_ENCRYPTION_INDICATORS =
-  'http://localhost:1234/api/message_encryption_indicators';
+  `http://${process.env.API_HOST}:1234/api/message_encryption_indicators`;
--- src/config/ipfs.js
+++ src/config/ipfs.js
@@ -1,4 +1,4 @@
 const IPFS_CONFIG = {
-  API_ENDPOINT: 'http://127.0.0.1:5001'
+  API_ENDPOINT: `http://${process.env.IPFS_HOST}:5001`
 };
 export default IPFS_CONFIG;
